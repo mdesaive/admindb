1882566719aa0024ddb19eebc36c065ba8da6eb8
Melanie Desaive <desaive@gmx.de>
2016-03-12 22:08:12 +0100

    PEP8 check and update license information.

--- admindb/admindb/settings.py
+++ admindb/admindb/settings.py
@@ -9,12 +9,25 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 
-Author: Melanie Desaive
+Author: Melanie Desaive, desaive@gmx.de
 Copyrigh (c) 2016, Melanie Desaive
 All rights reserved.
 
 Licensed under the GNU General Public License.
 See: COPYING.txt in project root.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
--- admindb/itservices/models.py
+++ admindb/itservices/models.py
@@ -1,12 +1,26 @@
 """
 Model definition for app itservice.
 
-Author: Melanie Desaive
+
+Author: Melanie Desaive, desaive@gmx.de
 Copyrigh (c) 2016, Melanie Desaive
 All rights reserved.
 
 Licensed under the GNU General Public License.
 See: COPYING.txt in project root.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from django.db import models
