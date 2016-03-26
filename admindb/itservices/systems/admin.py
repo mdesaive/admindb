b00c48044a4b6538ac1deaa683d28108ac158da6
Melanie Desaive <desaive@gmx.de>
2016-03-26 16:23:33 +0100

    Created choice box for host_id but trigger not hooked correctly yet.

--- admindb/itservices/systems/admin.py
+++ admindb/itservices/systems/admin.py
@@ -22,7 +22,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from django.contrib import admin
+
+# To define custom form
+from django import forms
+# from django.forms import select
+
 # from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.contenttypes.models import ContentType
 
 # To use genericadmin
 from genericadmin.admin import GenericAdminModelAdmin
@@ -116,7 +122,6 @@ class ClusterAdmin(admin.ModelAdmin):
     list_display = ('name', 'cluster_technology',
                     'description', 'itservice', 'landspace')
 
-
 class HostInstanceAdmin(admin.ModelAdmin):
     pass
     # fieldsets = [(None, {'fields': ['name', 'virtualization_technology', 'description', 'note', 'itservice', 'landspace']}),
@@ -129,13 +134,63 @@ class VirtualizationTechnologyAdmin(admin.ModelAdmin):
     inlines = (ContainerInline, VMInline)
 
 
+class ImportSystemsAggregatedForm(forms.ModelForm):
+    # Adding an extra field which enables to select the host
+    # system ID and disable automatic field.
+    # Maybe delete, was a test.
+    extra_field = forms.ChoiceField()
+
+    def fill_host_id_choices(self):
+    """Fills in a list of valid choices depending on selected host_type
+
+    Has to be hooked to the right place. Continue HERE! 
+    """
+        # Read out the host id selected.
+        my_host_id = self['host_type'].value()
+        print("Hello here! ****** " + str(my_host_id))
+
+        # Retrieve the ContentType object for the selected ID. 
+        my_host_type = ContentType.objects.get(pk=my_host_id)
+        print("Hello here! ****** " + str(my_host_type))
+
+        # Retrieve the model for the ContentType object.
+        my_host_model = my_host_type.model_class()
+        print("models: " + str(my_host_model))
+
+        # Fill in the values in the choice field.
+        self.fields['extra_field'].choices = [(x.pk, str(x)) for x in my_host_model.objects.all()]
+
+    def save(self, commit=True):
+        extra_field = self.cleaned_data.get('extra_field', None)
+        self.fill_host_id_choices()
+        return super(ImportSystemsAggregated, self).save(commit=commit)
+
+    def __init__(self, *args, **kwargs):
+        super(ImportSystemsAggregatedForm, self).__init__(*args, **kwargs)
+        # Overriding to define custom form
+        self.fields['host_id'].widget = forms.Select()
+        # self.fields['host_id'].choices = [('1',), ('2',),('3',), ]
+        # self.fields['host_id'].choices = [(x.pk, str(x)) for x in Computer.objects.all()]
+        self.fill_host_id_choices()
+
+    class Meta:
+        model = ImportSystemsAggregated
+        exclude = ('note',)
+
+
 class ImportSystemsAggregatedAdmin(GenericAdminModelAdmin):
+    # Uses my custom form defined above.
+    form = ImportSystemsAggregatedForm
+
     list_display = ('name', 'check1', 'check2', 'itservice', 'group', 'landspace', 'tag1', 'tag2', 'tag3')
     generic_fk_fields = [{
         'ct_field': 'host_type',
         'fk_field': 'host_id',
         }]
     content_type_whitelist = ('computer', 'cluster')
+    # formfield_overrides = {
+    #     models.TextField: {'widget': RichTextEditorWidget},
+    # }
 
 
 class VMAdmin(GenericAdminModelAdmin):
